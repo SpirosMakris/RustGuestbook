78092001a33656c8a1a9583b7e39246bedb4bb3f
Spiros Makris <makris.spiros@gmail.com>
2019-01-12 14:53:17 +0200

    Added database support (insert)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
-extern crate rocket_contrib;
+#[macro_use] extern crate rocket_contrib;
+extern crate chrono;
 
 use std::collections::HashMap;
 use rocket_contrib::templates::Template;
+use chrono::Utc;
+
+use rocket_contrib::databases::rusqlite;
+#[database("db_guestbook")]
+struct GuestbookDbConn(rusqlite::Connection);
+
+#[derive(FromForm)]
+struct Post {
+    name: String,
+    title: String,
+    content: String,
+}
+
+use rocket::request::Form;
+use rocket::response::Redirect;
 
 #[get("/")]
 fn index() -> Template {
@@ -21,10 +37,23 @@ fn topic_form() -> Template {
     Template::render("topic_form", context)
 }
 
+#[post("/topic", data="<post>")]
+fn create_topic(conn: GuestbookDbConn, post: Form<Post>) -> Redirect {
+    // let post_data = post.get();
+    conn.execute(
+        "INSERT INTO post (name, title, content, created_time) VALUES (?1, ?2, ?3, ?4)",
+        &[&post.name, &post.title, &post.content, &Utc::now().naive_utc().to_string()]
+    ).unwrap();
+
+    Redirect::to("/")
+}
+
 fn main() {
     rocket::ignite()
         .mount("/", routes![index])
         .mount("/", routes![topic_form])
+        .mount("/", routes![create_topic])
         .attach(Template::fairing())
+        .attach(GuestbookDbConn::fairing())
         .launch();
 }
