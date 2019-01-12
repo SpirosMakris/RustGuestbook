c3bcd4f334d981b576012d554aaae0c739217d69
Spiros Makris <makris.spiros@gmail.com>
2019-01-12 12:43:01 +0200

    Added handlebard template support

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+extern crate rocket_contrib;
+
+use std::collections::HashMap;
+use rocket_contrib::templates::Template;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    let mut context = HashMap::new();
+    context.insert("title", "Rust GuestBook");
+    context.insert("body", "Welcome to my guestbook.");
+    Template::render("index", context)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+        .launch();
 }
