6989ae8decdf32e9a7c500460d9279086879a726
Spiros Makris <makris.spiros@gmail.com>
2019-01-13 15:36:44 +0200

    Already was using struct for template context, fixed iter

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 #[macro_use] extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
 extern crate chrono;
+extern crate serde;
 
 use std::collections::HashMap;
 use rocket_contrib::templates::Template;
@@ -32,15 +33,7 @@ use rocket::response::Redirect;
 
 #[get("/")]
 fn index(conn: GuestbookDbConn) -> Template {
-    // let mut context = HashMap::new();
-    // context.insert("title", "Rust GuestBook");
-    // context.insert("body", "Welcome to my guestbook.");
-
-    let mut context = TemplateContext {
-        title: "Rust Guestbook!",
-        index_content: Some("Welcome to my guestbook".to_string()),
-        posts: Vec::new(),
-    };
+    
   // Make an sql statement and apply a closure to executed result -> iterator
     let mut stmt = conn.prepare("SELECT name, title, content FROM post").unwrap();
     let post_iter = stmt.query_map(&[],
@@ -53,25 +46,14 @@ fn index(conn: GuestbookDbConn) -> Template {
        } 
     ).unwrap();
 
-    for post in post_iter {
-        context.posts.push(post.unwrap());
-    }
+    let context = TemplateContext {
+        title: "Rust Guestbook!",
+        index_content: Some("Welcome to my guestbook".to_string()),
+        posts: post_iter.map(
+            |post| post.unwrap()
+        ).collect(),
+    };
 
-    // let mut post_content = String::new();
-    // for post in post_iter {
-    //     let mut post_context = HashMap::new();
-    //     let post = post.unwrap(); // Unwrap because it's a result
-    //     post_context.insert("name", &post.name);
-    //     post_context.insert("title", &post.title);
-    //     // post_context.insert("content", &post.content);
-        
-    //     // post_context.insert("parent", &"post".to_string());
-    //     Template::render("post", post_context);
-    //     // post_content.push_str(Template::render("post", post_context).value.unwrap().as_str());
-    //     // context.
-    // }
-
-    
     Template::render("index", context)
 }
 
@@ -84,7 +66,6 @@ fn topic_form() -> Template {
 
 #[post("/topic", data="<post>")]
 fn create_topic(conn: GuestbookDbConn, post: Form<Post>) -> Redirect {
-    // let post_data = post.get();
     conn.execute(
         "INSERT INTO post (name, title, content, created_time) VALUES (?1, ?2, ?3, ?4)",
         &[&post.name, &post.title, &post.content, &Utc::now().naive_utc().to_string()]
